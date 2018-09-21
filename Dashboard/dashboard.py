8f8862a0aca0f40d3b15a62735951ca1d0f9036b
gudgud96 <helloharry66@gmail.com>
2018-09-21 11:45:31 +0800

    dashboard commit

---
+++ Dashboard/dashboard.py
@@ -0,0 +1,9 @@
+from flask import render_template, redirect, url_for, request, session, flash, Blueprint
+from Dashboard.utils import login_required
+
+dashboard_api = Blueprint('dashboard', __name__, template_folder='templates')
+
+@dashboard_api.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')  # render a template
