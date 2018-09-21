ca1715ac0de2b15c2acf0d09c1a03c85a56817dd
gudgud96 <helloharry66@gmail.com>
2018-09-21 10:35:51 +0800

    Revert "dashboard commit"
    This reverts commit c34bff4d5155437a02f2ecfc70127ca61737031c.

--- Dashboard/dashboard.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, redirect, url_for, request, session, flash, Blueprint
-from utils import login_required
-
-dashboard_api = Blueprint('dashboard', __name__, template_folder='templates')
-
-@dashboard_api.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html')  # render a template
