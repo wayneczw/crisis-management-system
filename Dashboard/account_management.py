bd97f78369c7c988f7f9154b31bedbd120aa2eaa
gudgud96 <haohao@visenze.com>
2018-09-22 19:04:37 +0800

    register page commit

--- Dashboard/account_management.py
+++ Dashboard/account_management.py
@@ -1,16 +1,14 @@
 from flask import render_template, redirect, url_for, request, session, flash, Blueprint
 from Dashboard.utils import login_required
 
-dashboard_api = Blueprint('dashboard', __name__, template_folder='templates')
+account_api = Blueprint('account', __name__, template_folder='templates')
 
-@dashboard_api.route('/dashboard')
+@account_api.route('/register')
 @login_required
-def dashboard():
-    return render_template('dashboard.html')  # render a template
+def register():
+    return render_template('register.html')  # render a template
 
-
-#
-@dashboard_api.route('/deregister')
+@account_api.route('/deregister')
 @login_required
 def deregister():
     return render_template('deregister.html')  # render a template
