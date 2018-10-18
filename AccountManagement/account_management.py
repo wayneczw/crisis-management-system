cdd391d26d32db0f052bc192a0900152f2df254a
gudgud96 <helloharry66@gmail.com>
2018-10-18 17:40:34 +0800

    Add account management UTs

--- AccountManagement/account_management.py
+++ AccountManagement/account_management.py
@@ -5,8 +5,8 @@ from utils import login_required
 
 account_api = Blueprint('account', __name__, template_folder='templates')
 
-from app import db
 from model import *
+import re
 
 
 @account_api.route('/register', methods=['GET', 'POST'])
@@ -14,9 +14,18 @@ from model import *
 def register():
     if request.method == "POST":
         new_username, new_password, new_role = request.form['hf-username'], request.form['hf-password'], request.form['select']
-        __register_user(new_username, new_password, new_role)
-        flash('User - {} of role - {} is successfully created.'.format(new_username, new_role))
+        status = register_user(new_username, new_password, new_role)
+        if status == 0:
+            flash("Username must contain 8 characters with small letters and numbers.")
+        elif status == 1:
+            flash('User - {} of role - {} is successfully created.'.format(new_username, new_role))
+        elif status == 2:
+            flash("Password must contain at least 10 characters with uppercase, lowercase letters and numbers.")
+        else:
+            flash("Register failed. User already exists.")
+
     return render_template('register.html')  # render a template
+
 
 @account_api.route('/deregister', methods=['GET', 'POST'])
 @login_required
@@ -24,28 +33,39 @@ def deregister():
     if request.method == "POST":
         try:
             username, is_checked = request.form['username'], request.form['checkbox1']
-            __deregister_user(username)
+            deregister_user(username)
             flash('User - {} is successfully deregistered.'.format(username))
         except BadRequestKeyError:
-            flash('Please confirm that you want to deregister this user!')
+            flash('User does not exist. Please try again.')
     return render_template('deregister.html')  # render a template
 
 
-def __register_user(new_username, new_password, new_role):
-    # open('db/userlist.csv', 'a+').write("{},{},{}\n".format(new_username, new_password, new_role))
-    u = User(username=new_username, password=new_password,
-             role_id=Role.query.filter_by(name=new_role).first().id)
-    db.session.add(u)
-    db.session.commit()
+def register_user(new_username, new_password, new_role):
+    # username must contain 8 characters, small letters and numbers
+    if not re.match("^(?=.*[a-z])(?=.*\d)[a-z\d]{8}$", new_username):
+        return 0        # username violation
+
+    # password must contain 10 characters, including big, small letters and numbers
+    elif not re.match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10,}$", new_password):
+        return 2        # password violation
+
+    else:
+        try:
+            u = User(username=new_username, password=new_password,
+                     role_id=Role.query.filter_by(name=new_role).first().id)
+            db.session.add(u)
+            db.session.commit()
+            return 1    # OK
+        except Exception as e:
+            return 3    # exception, user already exist
 
 
-def __deregister_user(username):
-    # userlist = open('db/userlist.csv').readlines()
-    # for line in userlist:
-    #     if username in line:
-    #         userlist.remove(line)
-    #         break
-    # open('db/userlist.csv', 'w+').writelines(userlist)
-    u = User.query.filter_by(username=username).first()
-    db.session.delete(u)
-    db.session.commit()
+def deregister_user(username):
+    try:
+        u = User.query.filter_by(username=username).first()
+        db.session.delete(u)
+        db.session.commit()
+        return True
+
+    except Exception as e:
+        return False    # exception, user does not exist
